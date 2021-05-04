6eef5be6577c357f4b7f9c8b58e6e2fcc2e43af5
kunalpohakar <kunalpohakar242@gmail.com>
2021-05-04 17:03:55 +0530

    Logical Operator and Comparison Operator

---
+++ Python basics 2/Logical Operator/Logical_Comparison_operator.py
@@ -0,0 +1,31 @@
+# Logical Operator and Comparison Operators
+"""
+Comparison Operators are :
+< : less than
+> : greater than
+<= : less than equal to
+>= : greater than equal to
+== : equal to equal to
+!= : not equal to
+
+Logical Operators are :
+and
+or
+not
+
+"""
+# Comparison Operators
+print(1 < 2)
+print(1 > 2)
+print(1 <= 0)
+print(1 >= 0)
+print(1 == 1)
+print(1 != 1)
+
+# Logical Operators
+a = True
+b = False
+
+print(a and b)
+print(a or b)
+print(not a)
