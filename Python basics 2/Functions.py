d9c70e23f2364045b761095730cbd27c29468853
kunalpohakar <kunalpohakar242@gmail.com>
2021-05-15 19:16:31 +0530

    functions

---
+++ Python basics 2/Functions.py
@@ -0,0 +1,30 @@
+# Functions
+# syntax of creating a function
+'''
+def function_name():
+    body of function
+    
+function_name()
+
+'''
+
+def say_hello():
+    print('Hellooooo')
+    
+say_hello()
+
+'''
+Functions allow us to keep a code DRY and you can re-use and call a function multiple times. 
+'''
+
+# Arguments vs Parameters
+
+# Parameters we give when creating a function and 
+# Arguments we give when we call the function
+
+# parameters
+def main(name, emoji):
+    print(f'Hello {name} {emoji}')
+
+# Arguments
+main('Kunal', ':)')
