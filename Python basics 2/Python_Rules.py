1f2af60350655a4047ca91e33f119d0e587822fe
kunalpohakar <kunalpohakar242@gmail.com>
2021-05-22 13:31:30 +0530

    Create Python_Rules.py

---
+++ Python basics 2/Python_Rules.py
@@ -0,0 +1,12 @@
+# in this file we're gonna see some various python rules / key points.
+
+# 1. Doc String
+
+def test(a):
+    '''
+    In this function we print 'a' which is 10 and this comment is means DOc String.
+    '''
+    print(a)
+
+
+print(test.__doc__)
