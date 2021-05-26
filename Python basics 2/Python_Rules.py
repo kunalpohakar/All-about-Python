0447411dbb5c8168f043278bbf10ea33e91676f6
kunalpohakar <kunalpohakar242@gmail.com>
2021-05-26 20:36:22 +0530

    started oop concepts

--- Python basics 2/Python_Rules.py
+++ Python basics 2/Python_Rules.py
@@ -10,3 +10,17 @@ def test(a):
 
 
 print(test.__doc__)
+
+
+# Inside the Assignment Expression there is a new operator which is Walrus Operator.
+
+# := that assign a value to a variables as part of a larger expression.
+
+a = 'hellooooooooooo'
+
+if (n := len(a)) > 8:
+    print(f"Leanth of a is {n}")
+    
+while (n := len(a)) > 1:
+    print(n)
+    a = a[:-1]
